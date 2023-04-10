94697661bab0df8931f801cf26987b8254d06209
Ben Gosney <bengosney@googlemail.com>
2023-04-10 21:36:15 +0100

    it traces nothing

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,37 @@ import { useCallback, useRef } from "react";
 import "./App.css";
 import Canvas from "./Canvas";
 import useMaxSize, { ASPECT_4_3 } from "./hooks/useMaxSize";
-import { Vec2, Vec3, vec2 } from "./utils/math";
-import { RGB, rgb } from "./utils/colour";
+import { Vec2, Vec3, add, vec2, vec3, normalize, mul } from "./utils/math";
+import { RGB, rgb, vec3ToRGB } from "./utils/colour";
+
+type Shape = "sphere";
+
+interface Object {
+  shape: Shape;
+  position: Vec3;
+  colour: RGB;
+}
+
+interface Sphere extends Object {
+  shape: "sphere";
+  radius: number;
+}
+
+type Objects = Sphere;
+
+const objects: Objects[] = [
+  {
+    shape: "sphere",
+    radius: 3,
+    position: vec3(3, 7, 7),
+    colour: rgb(255, 0, 0),
+  },
+];
 
 function App() {
   const { width, height } = useMaxSize(ASPECT_4_3);
+  const focalLength = 50;
+  const samples = 100;
   const imageData = useRef<ImageData>();
 
   const drawPixel = useCallback(
@@ -28,9 +54,23 @@ function App() {
     imageData.current = context.createImageData(width, height);
   }, []);
 
+  const trace = (origin: Vec3, direction: Vec3, objects: Objects[], steps: number): Vec3 => {
+    return vec3(0, 0, 0);
+  };
+
   const frame = useCallback((context: CanvasRenderingContext2D, since: number) => {
-    for (let i = 0; i < Math.min(width, height); i++) {
-      drawPixel(vec2(i, i), rgb(255, 0, 0));
+    for (let x = 0; x < width; x++) {
+      for (let y = 0; y < height; y++) {
+        const direction = normalize(vec3(x, y, focalLength));
+
+        let color = vec3(0, 0, 0);
+        for (let i = 0; i < samples; i++) {
+          color = add(color, trace(vec3(0, 0, 0), direction, objects, 4));
+        }
+        color = mul(color, 1 / samples);
+
+        drawPixel({ x, y }, vec3ToRGB(color));
+      }
     }
 
     if (imageData.current !== undefined) {
--- src/utils/colour.ts
+++ src/utils/colour.ts
@@ -1,3 +1,5 @@
+import { Vec3, vec3 } from "./math";
+
 export interface RGB {
   r: number;
   g: number;
@@ -5,5 +7,8 @@ export interface RGB {
 }
 
 export const rgb = (r: number, g: number, b: number): RGB => ({ r, g, b });
+
+export const rgbToVec3 = ({ r, g, b }: RGB): Vec3 => vec3(r, g, b);
+export const vec3ToRGB = ({ x, y, z }: Vec3): RGB => rgb(x, y, z);
 
 export const RGBToHex = ({ r, g, b }: RGB): string => "#" + ((1 << 24) | (r << 16) | (g << 8) | b).toString(16).slice(1);
