53d6847e37a67b72abf057d921aa115b8229b80c
Ben Gosney <bengosney@googlemail.com>
2023-04-15 10:45:26 +0100

    Remove some of the stuff moved to rust

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useCallback, useEffect, useRef, useState } from "react";
 import "./App.css";
 import Canvas from "./Canvas";
 import useMaxSize, { ASPECT_4_3 } from "./hooks/useMaxSize";
-import { Vec2, Vec3, add, vec3, normalize, mul, sub, mag, dot, reflect, mulParts, avg, vec2 } from "./utils/math";
+import { Vec3, vec3 } from "./utils/math";
 import { RGB, rgb, rgbToVec3, vec3ToRGB } from "./utils/colour";
 
 import initWASM, { Scene, Entity, Shape as wasmShape, Vec3 as wasmVec3, RGB as wasmRGB } from "wasm-lib";
@@ -89,135 +89,6 @@ const objects: Sceen = [
   },
 ];
 
-interface IntersectionResult {
-  collided: boolean;
-  dist: number;
-  point: Vec3;
-  normal: Vec3;
-  object?: Objects;
-}
-
-const sphereIntersection = (origin: Vec3, direction: Vec3, sphere: Sphere): IntersectionResult => {
-  const sphereRay = sub(sphere.position, origin);
-  const distSphereRay = mag(sphereRay);
-  const distToClosestPointOnRay = dot(sphereRay, direction);
-  const distFromClosestPointToSphere = Math.sqrt(distSphereRay ** 2 - distToClosestPointOnRay ** 2);
-
-  const distToIntersection =
-    distToClosestPointOnRay - Math.sqrt(Math.abs(sphere.radius ** 2 - distFromClosestPointToSphere ** 2));
-  const point = add(origin, mul(direction, distToIntersection));
-  let normal = normalize(sub(point, sphere.position));
-
-  normal = normalize(
-    add(normal, mul(vec3(Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5), sphere.roughness)),
-  );
-
-  if (distToClosestPointOnRay > 0 && distFromClosestPointToSphere < sphere.radius) {
-    return {
-      collided: true,
-      dist: distToIntersection,
-      point: point,
-      normal: normal,
-    };
-  }
-
-  return {
-    collided: false,
-    dist: Infinity,
-    point: vec3(0, 0, 0),
-    normal: vec3(0, 0, 0),
-  };
-};
-
-const intersection = (origin: Vec3, direction: Vec3, sceen: Sceen): IntersectionResult => {
-  const closestIntersection: IntersectionResult = {
-    collided: false,
-    point: vec3(0, 0, 0),
-    dist: Infinity,
-    normal: vec3(0, 0, 0),
-    object: undefined,
-  };
-
-  sceen.forEach((object) => {
-    switch (object.shape) {
-      case "sphere":
-        const intersection = sphereIntersection(origin, direction, object);
-
-        if (intersection.dist < closestIntersection.dist) {
-          closestIntersection.dist = intersection.dist;
-          closestIntersection.object = object;
-          closestIntersection.normal = intersection.normal;
-          closestIntersection.point = intersection.point;
-        }
-
-        closestIntersection.collided = closestIntersection.collided || intersection.collided;
-        break;
-    }
-  });
-
-  return closestIntersection;
-};
-
-const trace = (origin: Vec3, direction: Vec3, sceen: Sceen, steps: number): Vec3 => {
-  const intersect = intersection(origin, direction, sceen);
-
-  if (intersect.collided && steps > 0 && intersect.object !== undefined) {
-    const reflectedDirection = reflect(direction, intersect.normal);
-
-    const bounce = trace(
-      intersect.point,
-      reflectedDirection,
-      objects.filter((o) => o != intersect.object),
-      steps - 1,
-    );
-
-    return add(rgbToVec3(intersect.object?.emission), mulParts(bounce, rgbToVec3(intersect.object.reflectivity)));
-  }
-
-  return vec3(0, 0, 0);
-};
-
-const render = (
-  width: number,
-  height: number,
-  focalLength: number,
-  sampleCount: number,
-  bounces: number,
-  drawPixel: any,
-  [start, finish]: [any, any],
-) => {
-  const halfWidth = Math.floor(width / 2);
-  const halfHeight = Math.floor(height / 2);
-  const origin = vec3(0, 0, 0);
-  const samples: Vec3[][][] = [];
-  const timeouts: ReturnType<typeof setTimeout>[] = [];
-
-  for (let s = 0; s < sampleCount; s++) {
-    start();
-    const timeout = setTimeout(() => {
-      for (let i = 0; i < width; i++) {
-        for (let j = 0; j < height; j++) {
-          const x = i - halfWidth;
-          const y = j - halfHeight;
-          const direction = normalize(vec3(x, y, focalLength));
-
-          samples[x] = samples[x] || [];
-          samples[x][y] = samples[x][y] || [];
-
-          samples[x][y].push(trace(origin, direction, objects, bounces));
-          const colour = avg(samples[x][y]);
-
-          //drawPixel({ x: i, y: j }, vec3ToRGB(colour));
-        }
-      }
-      finish();
-    }, 1);
-    timeouts.push(timeout);
-  }
-
-  return timeouts;
-};
-
 function App() {
   //const { width, height } = useMaxSize(ASPECT_4_3);
   const width = 320;
@@ -225,17 +96,12 @@ function App() {
   const focalLength = 50;
   const samples = 3;
   const bounces = 4;
-  const imageData = useRef<ImageData>();
-  const [ready, setReady] = useState<boolean>(false);
-  const [samplesStarted, setSamplesStarted] = useState<number>(0);
-  const addSampleStarted = () => setSamplesStarted((s) => s + 1);
-  const [samplesFinished, setSamplesFinished] = useState<number>(0);
-  const addSampleFinished = () => setSamplesFinished((s) => s + 1);
+  const [context, setContext] = useState<CanvasRenderingContext2D>();
 
   useEffect(() => {
-    if (imageData.current) {
+    if (context) {
       initWASM().then(() => {
-        const scene = new Scene(320, 240, 50, 10, 4);
+        const scene = new Scene(context.canvas.width, context.canvas.height, focalLength, samples, bounces);
         objects.forEach((o) => {
           const entity = new Entity(
             wasmShape.Sphere,
@@ -248,56 +114,18 @@ function App() {
           scene.add_entity(entity);
         });
 
-        const pixels = scene.render();
-        if (imageData.current !== undefined) {
-          for (let i = 0; i < pixels.length; i++) {
-            imageData.current.data[i] = pixels[i];
-          }
-        }
+        scene.render(context);
       });
     }
-  }, [ready, imageData.current]);
-
-  const drawPixel = useCallback(
-    ({ x, y }: Vec2, colour: RGB) => {
-      const offset = 4 * (Math.floor(x) + Math.floor(y) * width);
-      if (imageData.current !== undefined) {
-        imageData.current.data[offset] = colour.r;
-        imageData.current.data[offset + 1] = colour.g;
-        imageData.current.data[offset + 2] = colour.b;
-        imageData.current.data[offset + 3] = 255;
-      }
-    },
-    [width],
-  );
+  }, [context]);
 
   const init = useCallback((context: CanvasRenderingContext2D) => {
-    const width = Math.floor(context.canvas.width);
-    const height = Math.floor(context.canvas.height);
-    imageData.current = context.createImageData(width, height);
-    setReady(true);
-  }, []);
-
-  useEffect(() => {
-    if (imageData.current && false) {
-      const timeouts = render(width, height, focalLength, samples, bounces, drawPixel, [
-        addSampleStarted,
-        addSampleFinished,
-      ]);
-
-      return () => timeouts.forEach((t) => clearTimeout(t));
-    }
-  }, [ready, imageData.current]);
-
-  const frame = useCallback((context: CanvasRenderingContext2D, since: number) => {
-    if (imageData.current !== undefined) {
-      context.putImageData(imageData.current, 0, 0);
-    }
+    setContext(context);
   }, []);
 
   return (
     <div className="App">
-      <Canvas animating={true} width={width} height={height} init={init} frame={frame} />
+      <Canvas animating={false} width={width} height={height} init={init} frame={() => {}} />
     </div>
   );
 }
