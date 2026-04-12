a5b82e3fa604ac190a5bd9090469586628918e73
Ben Gosney <bengosney@googlemail.com>
2026-04-12 21:34:45 +0100

    feat(stats): add 1 dp of percission to the seconds

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -2,14 +2,15 @@ export const humanDuration = (ms: number) => {
   if (Number.isNaN(ms)) return "NaN";
   if (ms <= 0) return "0ms";
 
-  const seconds = Math.floor((ms / 1000) % 60);
+  const totalSeconds = (ms / 1000) % 60;
+  const seconds = Math.floor(totalSeconds);
   const minutes = Math.floor((ms / (1000 * 60)) % 60);
   const hours = Math.floor(ms / (1000 * 60 * 60));
 
   const parts = [];
   if (hours > 0) parts.push(`${hours}h`);
   if (minutes > 0) parts.push(`${minutes}m`);
-  if (seconds > 0) parts.push(`${seconds}s`);
+  if (seconds > 0) parts.push(seconds < 10 ? `${totalSeconds.toFixed(1)}s` : `${seconds}s`);
   if (parts.length === 0) parts.push(`${ms.toFixed(0)}ms`);
 
   return parts.join(" ");
