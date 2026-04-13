b034cd85b38126300ac8b83853cb6595d1da113c
Ben Gosney <bengosney@googlemail.com>
2026-04-13 21:35:39 +0100

    fix(humanDuration): seconds should only show decimal when there are only seconds

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -6,11 +6,12 @@ export const humanDuration = (ms: number) => {
   const seconds = Math.floor(totalSeconds);
   const minutes = Math.floor((ms / (1000 * 60)) % 60);
   const hours = Math.floor(ms / (1000 * 60 * 60));
+  const showDecimal = seconds < 10 && minutes == 0;
 
   const parts = [];
   if (hours > 0) parts.push(`${hours}h`);
   if (minutes > 0) parts.push(`${minutes}m`);
-  if (seconds > 0) parts.push(seconds < 10 ? `${totalSeconds.toFixed(1)}s` : `${seconds}s`);
+  if (seconds > 0) parts.push(showDecimal ? `${totalSeconds.toFixed(1)}s` : `${seconds}s`);
   if (parts.length === 0) parts.push(`${ms.toFixed(0)}ms`);
 
   return parts.join(" ");
